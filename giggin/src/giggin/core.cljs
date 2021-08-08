e47c73f3b514d31c99a78d79b15db506ab98e6d8
tingtinghsu <tingtinghsu.tw@gmail.com>
2021-08-08 20:56:14 +0800

    Study: Compare js and cljs in core

--- giggin/src/giggin/core.cljs
+++ giggin/src/giggin/core.cljs
@@ -1,9 +1,18 @@
 (ns giggin.core
   (:require [reagent.core :as r]))
 
+;; function app() { return (<div className="container"></div>)}
 (defn app
   []
   [:div.container])
+
+(defn app
+  []
+  [:div {:class "container"}])
+
+(defn plus
+  [x y]
+  (+ x y))
 
 (defn ^:export main
   []
